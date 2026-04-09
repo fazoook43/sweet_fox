3b677a78a0a218a2d04e914e1341db4e39cf94e5
Mostafa Mohamed <mostafameesr@gmail.com>
2026-04-09 17:48:08 +0000

    Change to beta 0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -80,7 +80,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
 
 	# Fox Settings
-	export FOX_MAINTAINER_PATCH_VERSION="3"
+	export FOX_MAINTAINER_PATCH_VERSION="0"
 	export OF_MAINTAINER="Mostafameesr"
 	export FOX_BUILD_TYPE="Beta"
 
